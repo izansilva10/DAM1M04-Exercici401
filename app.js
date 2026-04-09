97b9f066cfdbf27137d0f72f15cf0c5cb487f3ef
izansilva10 <izaansilvaa10@gmail.com>
2026-04-09 18:02:34 +0200

    minim app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionant correctament');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor a http://localhost:${PORT}`);
+});
